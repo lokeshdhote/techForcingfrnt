11f1a908166f45f22941bff8d0d7d3581b95e1ec
lokeshdhote <lokeshdhote2004@gmail.com>
2025-05-08 23:23:34 +0530

    three

--- utils/Axios.jsx
+++ utils/Axios.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL:"https://techforcinfbcknd.onrender.com/api",
-    baseURL:" http://localhost:5000/api",
+    // baseURL:" http://localhost:5000/api",
 
     withCredentials:true
 })
